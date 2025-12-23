cb60bc70d30a368d5ed582e56308e6830a7e3d8b
minhminh03 <125256910+HaiMinhDoan@users.noreply.github.com>
2025-12-23 16:43:17 +0700

    sửa mail công ty

--- src/main/java/com/devmam/taraacademyapi/config/MailConfig.java
+++ src/main/java/com/devmam/taraacademyapi/config/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("thientm01@gmail.com");
-        mailSender.setPassword("nwbn bihl bexf ifau"); // <- App Password
+        mailSender.setUsername("sale@slmsolar.com");
+        mailSender.setPassword("vvdl swax vcny type"); // <- App Password
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
